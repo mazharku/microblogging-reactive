1fb428f762be1e6a3d4829131169d0dae5e43ba7
mazharku <mazhar.shapnil@gmail.com>
2021-05-02 20:12:33 +0600

    setup file added

---
+++ src/main/java/com/mazhar/reactive/MicrobloggingReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.mazhar.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobloggingReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobloggingReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mazhar/reactive/MicrobloggingReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazhar.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobloggingReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
